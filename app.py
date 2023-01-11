0898c4e5db4d0e7fbfa5b4ce3f2ca7c1c12ac921
Yunwltn <120348555+Yunwltn@users.noreply.github.com>
2023-01-11 13:04:15 +0900

    add

--- app.py
+++ app.py
@@ -1,18 +1,34 @@
-from flask import Flask, jsonify, make_response
+from flask import Flask
+from flask_jwt_extended import JWTManager
+from flask_restful import Api
+from config import Config
+from resources.follow import FollowResource
+from resources.memo import MemoListResource, MemoResource, FollowMemoListResource
+from resources.user import UserLoginResource, UserRegisterResource, UserLogoutResource
+from resources.user import jwt_blacklist
 
 app = Flask(__name__)
 
+app.config.from_object(Config)
 
-@app.route("/")
-def hello_from_root():
-    return jsonify(message='우왕')
+jwt = JWTManager(app)
 
+@jwt.token_in_blocklist_loader
+def check_if_token_is_revoked(jwt_header, jwt_payload) : 
+    jti = jwt_payload['jti']
+    return jti in jwt_blacklist
 
-@app.route("/hello")
-def hello():
-    return jsonify(message='Hello from path!')
+api = Api(app)
 
+api.add_resource(UserRegisterResource, '/user/register')
+api.add_resource(UserLoginResource, '/user/login')
+api.add_resource(UserLogoutResource, '/user/logout')
 
-@app.errorhandler(404)
-def resource_not_found(e):
-    return make_response(jsonify(error='Not found!'), 404)
+api.add_resource(MemoListResource, '/memo')
+api.add_resource(MemoResource, '/memo/<int:memo_id>')
+
+api.add_resource(FollowResource, '/follow/<int:followee_id>')
+api.add_resource(FollowMemoListResource, '/follow/memo')
+
+if __name__ == '__main__' :
+    app.run()
